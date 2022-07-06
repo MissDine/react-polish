0ec6ff24c6671e7248e31ef4263fb1d0e0a3d930
MissDine <enidnyatichi1212@gmail.com>
2022-07-06 15:30:41 +0300

    Feature edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,29 @@
 import { useState } from "react";
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title:"My life", body: "lorem...", author: "Enid",id:1},
+        {title:"My body", body: "lorem...", author: "Dine",id:2},
+        {title:"My family", body: "lorem...", author: "Tichi",id:31}
+    ])
     // let fname = "Enid";
-    const [fname, setFname] = useState("Israel");
-    const [age, setAge] = useState(8)
-    const handleClick = (e) =>{
-        // fname = "Norbert";
-        setFname("Norbert");
-        setAge(16)
-        console.log("Hello Norbert", e);
-    }
-    const handleClickAgain = (name, e) =>{
-        console.log("Hello"+name, e);
-    }
+    // const [fname, setFname] = useState("Israel");
+    // const [age, setAge] = useState(8)
+    // const handleClick = (e) =>{
+    //     // fname = "Norbert";
+    //     setFname("Norbert");
+    //     setAge(16)
+    //     console.log("Hello Norbert", e);
+    // }
+    // const handleClickAgain = (name, e) =>{
+    //     console.log("Hello"+name, e);
+    // }
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{"Good morning " + fname}</p>
+            {/* <p>{"Good morning " + fname}</p>
             <p>{fname} is {age} years old.</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick = {(e) => handleClickAgain(" Israel",e)}>Click me again</button>
+            <button onClick = {(e) => handleClickAgain(" Israel",e)}>Click me again</button> */}
         </div>
     );
 }
