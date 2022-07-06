c41d99905e1b47e34d9d44145efabfb454a257eb
MissDine <enidnyatichi1212@gmail.com>
2022-07-06 14:41:26 +0300

    Feature edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,14 @@ const Home = () => {
     const handleClick = () =>{
         console.log("Hello Norbert");
     }
+    const handleClickAgain = (name) =>{
+        console.log("Hello"+name);
+    }
     return (
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
+            <button onClick = {() => handleClickAgain(" Israel")}>Click me again</button>
         </div>
     );
 }
