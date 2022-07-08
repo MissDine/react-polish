fe564a7a118a7b17a4fcd5bc579f39558d81e2c9
MissDine <enidnyatichi1212@gmail.com>
2022-07-08 11:28:30 +0300

    Feature edit

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,6 +1,7 @@
-const BlogList = ({blogs,title}) => {
+const BlogList = ({blogs,title,handleDelete}) => {
     // const blogs = props.blogs
     // const title = props.title
+   
     return (
         <div className="bloglist">
         <h1>{title}</h1>
@@ -8,6 +9,7 @@ const BlogList = ({blogs,title}) => {
                 <div className="blog-preview" key = {blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={()=> handleDelete(blog.id)}>Delete blog</button>
                 </div>
             )
             )}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -6,6 +6,12 @@ const Home = () => {
         {title:"My body", body: "lorem...", author: "Dine",id:2},
         {title:"My family", body: "lorem...", author: "Tichi",id:31}
     ])
+    const [isPending, setPending] = useState(true)
+    const [error, setError] =useState("null")
+    const handleDelete = (id)=>{
+        const newBlogs = blogs.filter(blog =>blog.id !== id);
+        setBlogs(newBlogs)
+    }
     // let fname = "Enid";
     // const [fname, setFname] = useState("Israel");
     // const [age, setAge] = useState(8)
@@ -18,9 +24,36 @@ const Home = () => {
     // const handleClickAgain = (name, e) =>{
     //     console.log("Hello"+name, e);
     // }
+    useEffect(() =>{
+        console.log("Hey there");
+        console.log(blogs);
+    })
+    // UseEffect to fetch some data
+    useEffect(()=>{
+        // setTimeout(()=>{
+            fetch("http://localhost:8000/blogs")
+        .then(response => {
+            if(!response.ok){
+                throw Error("Could not fetch the data")
+            }
+            return response.json();
+        })
+        .then(data =>{
+            console.log(data);
+            setBlogs(data)
+            setPending(false)
+        })
+        .catch(err=>{
+            console.log(err.message);
+            setError(err.message)
+    })
+        // },1000)
+    }, [])
     return (
         <div className="home">
-            <BlogList blogs = {blogs} title = {"All blogs"}/>
+            {error && <div>{error}</div>}
+            {isPending && <div pend>loading</div>}
+            <BlogList blogs = {blogs} title = {"All blogs"} handleDelete= {handleDelete}/>
             <BlogList blogs = {blogs.filter((blog)=> blog.author ==="Enid" )} title = {"Enid's blogs"}/>
             {/* <h2>Homepage</h2> */}
             {/* <p>{"Good morning " + fname}</p>
